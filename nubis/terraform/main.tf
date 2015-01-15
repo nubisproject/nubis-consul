c7a8ac0ea4369bae2f204d7ee8801c41c6a62426
Philippe M. Chiasson <gozer@mozilla.com>
2015-01-15 15:43:32 -0500

    switch to using autoscaling for the non-bootstrap nodes

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -5,6 +5,30 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
+resource "aws_launch_configuration" "consul" {
+    name = "consul"
+    image_id = "${lookup(var.ami, var.region)}"
+    instance_type = "m3.medium"
+    key_name = "${var.key_name}"
+    security_groups = ["${aws_security_group.consul.name}"]
+
+    user_data = "CONSUL_DC=${var.region}\nCONSUL_TOKEN=${var.token}\nCONSUL_JOIN=${aws_instance.bootstrap.private_dns}\nCONSUL_BOOTSTRAP_EXPECT=$(( 1 + ${var.servers} ))"
+}
+
+resource "aws_autoscaling_group" "consul" {
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e"]
+  name = "consul"
+  max_size = "${var.servers}"
+  min_size = "${var.servers}"
+  health_check_grace_period = 10
+  health_check_type = "EC2"
+  desired_capacity = "${var.servers}"
+  force_delete = true
+  launch_configuration = "${aws_launch_configuration.consul.name}"
+}
+
+# Single node necessary for bootstrap and self-discovery
+# XXX: Problematic if it fails
 resource "aws_instance" "bootstrap" {
   ami = "${lookup(var.ami, var.region)}"
   
@@ -17,20 +41,6 @@ resource "aws_instance" "bootstrap" {
   }
 
   user_data = "CONSUL_DC=${var.region}\nCONSUL_TOKEN=${var.token}\nCONSUL_BOOTSTRAP_EXPECT=$(( 1 + ${var.servers} ))\n"
-}
-
-resource "aws_instance" "server" {
-  ami = "${lookup(var.ami, var.region)}"
-  instance_type = "m3.medium"
-  key_name = "${var.key_name}"
-  count = "${var.servers}"
-  security_groups = ["${aws_security_group.consul.name}"]
-  
-  tags {
-        Name = "Consul node"
-  }
-
-  user_data = "CONSUL_DC=${var.region}\nCONSUL_TOKEN=${var.token}\nCONSUL_JOIN=${aws_instance.bootstrap.private_dns}\nCONSUL_BOOTSTRAP_EXPECT=$(( 1 + ${var.servers} ))"
 }
 
 resource "aws_security_group" "consul" {
