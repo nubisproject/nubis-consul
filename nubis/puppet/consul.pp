3223b5c732a1094f9b1c7c04f8fa11fdc060f23e
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-03-04 21:07:06 -0500

    SSL enabled by default

--- nubis/puppet/consul.pp
+++ nubis/puppet/consul.pp
@@ -1,12 +1,18 @@
-class { 'consul':
-  version => "0.5.0",
+class { 'consul'=>
+  version => '0.5.0',
   
   config_hash => {
-      'data_dir'   => '/var/lib/consul',
-      'log_level'  => 'INFO',
-      'ui_dir'      => '/var/lib/consul/ui',
-      'client_addr' => '0.0.0.0',
-      'server'      => true,
-      'enable_syslog' => true,
+      'data_dir'        => '/var/lib/consul',
+      'log_level'       => 'INFO',
+      'ui_dir'          => '/var/lib/consul/ui',
+      'client_addr'     => '0.0.0.0',
+      'server'          => true,
+      'enable_syslog'   => true,
+      'ca_file'         => '/etc/consul/consul.pem',
+      'cert_file'       => '/etc/consul/consul.pem',
+      'key_file'        => '/etc/consul/consul.key',
+      'verify_incoming' => true,
+      'verify_outgoing' => true
+
   }
 }
