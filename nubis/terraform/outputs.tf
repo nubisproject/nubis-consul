99740a09bb43f6d12ed3fccfe58d29fbe2fcb3be
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-04-07 13:36:38 -0400

    Fix import of wrong module

--- nubis/terraform/outputs.tf
+++ nubis/terraform/outputs.tf
@@ -1,6 +1,13 @@
-output "internal-address" {
-  value = "http://${aws_route53_record.ui.fqdn}/"
-}
-output "public-address" {
-  value = "https://${aws_route53_record.public.fqdn}/"
+#output "internal-address" {
+#  value = "http://${aws_route53_record.ui.fqdn}/"
+#}
+#output "public-address" {
+#  value = "https://${aws_route53_record.public.fqdn}/"
+#}
+#output "elb-address" {
+#  value = "dualstack.${aws_elb.consul-public.dns_name}"
+#}
+
+output "iam_roles" {
+  value = "${join(",",aws_iam_role.consul.*.id)}"
 }
