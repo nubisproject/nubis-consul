cdf954f1352852934eb16f27cc943c35a2e495c3
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-06 15:03:46 -0400

    Install and enable consul_exporter, when node_exporter is enabled
    Fixes #208

---
+++ nubis/puppet/consul_exporter.pp
@@ -0,0 +1,44 @@
+$consul_exporter_version = "0.2.0"
+$consul_exporter_url = "https://github.com/prometheus/consul_exporter/releases/download/${consul_exporter_version}/consul_exporter-${consul_exporter_version}.linux-amd64.tar.gz"
+
+notice ("Grabbing consul_exporter ${consul_exporter_version}")
+staging::file { "consul_exporter.${consul_exporter_version}.tar.gz":
+  source => $consul_exporter_url,
+}->
+staging::extract { "consul_exporter.${consul_exporter_version}.tar.gz":
+  target  => "/usr/local/bin",
+  strip   => 1,
+  creates => "/usr/local/bin/consul_exporter",
+}
+
+file { '/etc/init/consul_exporter.conf':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  source  => 'puppet:///nubis/files/consul_exporter.upstart',
+}
+
+file { '/etc/init/consul_exporter.override':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  content => "manual",
+}
+
+file { '/etc/confd/conf.d/consul_exporter.toml':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  source  => 'puppet:///nubis/files/confd/conf.d/consul_exporter.toml',
+}
+
+file { '/etc/confd/templates/consul_exporter.tmpl':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  source  => 'puppet:///nubis/files/confd/templates/consul_exporter.tmpl',
+}
