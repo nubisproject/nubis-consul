102f1667d370549b5126b6a33b98430ffd5cf1da
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-10-04 14:40:07 -0400

    cleanup

--- nubis/terraform/outputs.tf
+++ nubis/terraform/outputs.tf
@@ -1,21 +1,7 @@
-#output "internal-address" {
-
-#  value = "http://${aws_route53_record.ui.fqdn}/"
-
-#}
-
-#output "public-address" {
-
-#  value = "https://${aws_route53_record.public.fqdn}/"
-
-#}
-
-#output "elb-address" {
-
-#  value = "dualstack.${aws_elb.consul-public.dns_name}"
-
-#}
-
 output "iam_roles" {
   value = "${join(",",aws_iam_role.consul.*.id)}"
+}
+
+output "master_acl_token" {
+  value = "${random_id.acl_token.hex}"
 }
